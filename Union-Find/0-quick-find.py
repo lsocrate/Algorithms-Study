32d197d2b7a97d569c7cd2adfc8b3cd8b9103a36
Luiz Sócrate <luiz@socrate.com.br>
2013-05-28 17:23:31 -0300

    Union FInd with Quick Find

---
+++ Union-Find/0-quick-find.py
@@ -0,0 +1,22 @@
+class UnionFind(object):
+    """Union Find Datatype"""
+
+    def __init__(self, N):
+        self.objects = list(range(N))
+
+    def connected(self, id1, id2):
+        return self.objects[id1] == self.objects[id2]
+
+    def union(self, id1, id2):
+        group1 = self.objects[id1]
+        group2 = self.objects[id2]
+        for id, group in enumerate(self.objects):
+            if group == group1:
+                self.objects[id] = group2
+
+a = UnionFind(10)
+a.union(1,2)
+a.union(7,8)
+a.union(1,8)
+
+print(a.connected(1, 7))
