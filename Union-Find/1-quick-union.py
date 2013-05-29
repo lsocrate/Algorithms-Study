c53f454be33e7ee9f723ca2e1b5927e838869e14
Luiz Sócrate <luiz@socrate.com.br>
2013-05-28 22:28:15 -0300

    Weighted quick union

--- Union-Find/1-quick-union.py
+++ Union-Find/1-quick-union.py
@@ -3,12 +3,21 @@ class UnionFind(object):
 
     def __init__(self, N):
         self.objects = list(range(N))
+        self.size = [1] * N
 
     def connected(self, id1, id2):
         return self._root(id1) == self._root(id2)
 
     def union(self, id1, id2):
-        self.objects[self._root(id1)] = self._root(id2)
+        rootId1 = self._root(id1)
+        rootId2 = self._root(id2)
+
+        if self.size[rootId1] < self.size[rootId2] :
+            self.objects[rootId1] = rootId2
+            self.size[rootId2] = self.size[rootId1] + self.size[rootId2]
+        else :
+            self.objects[rootId2] = rootId1
+            self.size[rootId1] = self.size[rootId1] + self.size[rootId2]
 
     def _root(self, id):
         while id is not self.objects[id]:
