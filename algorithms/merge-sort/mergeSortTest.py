efbf726f2556e3c34ada3ca9c15b0a86f3fb1f0e
Luiz Sócrate <luiz@socrate.com.br>
2013-07-04 02:38:17 -0300

    Reorganize merge sort tests

--- algorithms/merge-sort/mergeSortTest.py
+++ algorithms/merge-sort/mergeSortTest.py
@@ -4,18 +4,14 @@ from mergeSort import *
 class MergeSortTest(unittest.TestCase):
     """docstring for Example """
 
-    def test_merge_sort_simple(self):
+    def test_merge_sort_2n(self):
         mergeSort = MergeSort()
         result = mergeSort.sort([2,1])
         self.assertEqual([1,2], result)
         result2 = mergeSort.sort([1,2])
         self.assertEqual([1,2], result2)
-
-    def test_merge_sort_complex(self):
-        mergeSort = MergeSort()
-        result = mergeSort.sort([5,4,1,8,7,2,6,3])
-        self.assertEqual([1,2,3,4,5,6,7,8], result)
-
+        result3 = mergeSort.sort([5,4,1,8,7,2,6,3])
+        self.assertEqual([1,2,3,4,5,6,7,8], result3)
 
 if __name__ == '__main__':
     unittest.main()
